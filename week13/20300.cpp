6332495d4965275896ae3438ea11ca162f9eaa65
SangEun <olsang1203@gmail.com>
2024-03-04 12:22:06 +0900

    20300

--- week13/20300.cpp
+++ week13/20300.cpp
@@ -13,16 +13,19 @@ int main(){
         loss.push_back(l);
     }
     sort(loss.begin(),loss.end());
-    if(n%2==1){//홀수
-        min = loss[n-1];
-        n--;
+
+    if(n%2==1){//n: 홀수
+        min = loss[n-1];//마지막 값 저장
+        n--;//마지막 값(가장 큰 수) 제외
         for(int i = 0; i<n/2; i++){
+            //높은 값 + 낮은 값
             check = loss[i] + loss[n-1-i];
             if(check>min) min = check;
         }
     }
-    else{
+    else{//n:짝수
         for(int i = 0; i<n/2; i++){
+            //높은 값 + 낮은 값
             check = loss[i] + loss[n-1-i];
             if(check>min) min = check;
         }
