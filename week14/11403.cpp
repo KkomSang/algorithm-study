7abe732fb8e96289a065c7c96fd8e29c38be0b27
SangEun <olsang1203@gmail.com>
2024-03-11 21:35:38 +0900

    dfs/플로이드-와샬

--- week14/11403.cpp
+++ week14/11403.cpp
@@ -1,6 +1,8 @@
 #include <iostream>
 #include <vector>
 using namespace std;
+/* dfs */
+/*
 vector<int > v[100];
 int n;
 bool visited[101] = {false};
@@ -37,4 +39,31 @@ int main(){
         cout<<'\n';
     }
     return 0;
+}
+*/
+/*플로이드-와샬*/
+int g[101][101] = {0};
+int main(){
+    int n;
+    cin>>n;
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            cin>>g[i][j];
+        }
+    }
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            for(int k = 0; k<n; k++){
+                if(g[j][i] && g[i][k]){
+                    g[j][k] = 1;
+                }
+            }
+        }
+    }
+    for(int i = 0; i<n; i++){
+        for(int j = 0; j<n; j++){
+            cout<<g[i][j]<<" ";
+        }
+        cout<<'\n';
+    }
 }
